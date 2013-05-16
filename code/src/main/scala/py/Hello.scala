c596fdbe59d49f7056b2f8c07606a9baa4004860
SunPHM <pengyang823@gmail.com>
2013-05-16 17:12:26 -0400

    javacv hello

--- code/src/main/scala/py/Hello.scala
+++ code/src/main/scala/py/Hello.scala
@@ -1,6 +1,10 @@
 package py
+
+import com.googlecode.javacv.cpp.opencv_highgui._
+
 object Hello{
   def main(args:Array[String]){
     println("Hello World")
+    val image = cvLoadImage("boldt.jpg")
   }
 }
