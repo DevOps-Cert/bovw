5e2946be702136e7c8cc964cbe73bd8e87135457
Yang Peng <pengyang823@gmail.com>
2013-05-21 18:29:40 -0400

    fix compilation errro

---
+++ code/src/main/scala/py/Clustering.scala
@@ -0,0 +1,8 @@
+package py
+
+object Clustering {
+  // TODO: weka.jar and Mahout
+  // TODO: use top-down hierarchical clustering
+  // TODO: scale up the algorithm by using map-reduce paradigm
+  
+}
