57f1e5a169b66e8183a521957b693577c899ce3f
SunPHM <pengyang823@gmail.com>
2013-05-24 19:04:43 -0400

    store features into file

--- code/src/main/scala/py/Hello.scala
+++ code/src/main/scala/py/Hello.scala
@@ -2,6 +2,7 @@ package py
 
 import com.googlecode.javacv.cpp.opencv_highgui._
 import com.googlecode.javacv.cpp.opencv_core._
+import com.googlecode.javacv.cpp.opencv_core.CvMat
 import com.googlecode.javacv.cpp.opencv_features2d._
 import java.io.File
 import com.googlecode.javacv.CanvasFrame
@@ -17,22 +18,33 @@ import java.io.PrintWriter
 object Hello{
   def main(args:Array[String]){
     // loadFeatures("resources/images")
-    
     // test
-    
-    val image = cvLoadImage("boldt.jpg")   
+    val image = cvLoadImageM("boldt.jpg", CV_LOAD_IMAGE_GRAYSCALE)
     val keyPoints = new KeyPoint()
     val sift = new SIFT(0, 3, 0.03, 10, 1.6)
     sift.detect(image, null, keyPoints)
-    //val featureImage = cvCreateImage(cvGetSize(image), image.depth(), 3)
-    //drawKeypoints(image, keyPoints, featureImage, CvScalar.WHITE, DrawMatchesFlags.DRAW_RICH_KEYPOINTS)
-    //show(featureImage, "SIFT Features")
-    val points = toArray(keyPoints)
+    val des = sift.getDescriptorExtractor()
+    val cv_mat = new CvMat(null)
+    des.compute(image, keyPoints, cv_mat)
+    val pw = new PrintWriter(new File("resources/features.txt"))
+    println(cv_mat.cols())
+    
+/*    for(i <- 0 until cv_mat.cols())
+    {
+      val da = Array[Double]()
+      cv_mat.get(i, da)
+      for (j <- 0 until 512)
+       pw.println(cv_mat.get(i, j, 0))
+      //pw.println(da.mkString(" "))
+      //cv_mat.get(i)
+      pw.flush()
+    }*/
+    //cv_mat.ro
+    val points = toArray(keyPoints) // used to get all the positions and sizes of the key points
+    println(points.size)
     // pt i.e. position(x, y)
-    println("postion, angle, octave, response, size, capacity, limit")
-    points.foreach(p => {
-        println(p.pt() + ":" + p.angle() + ":" + p.octave() + p.response() + ":" + p.size() + ":" + p.capacity() + ":" + p.limit())
-    })
+    // println("postion, angle, octave, response, size, capacity, limit")
+    pw.close()
   }
   
   def extractKeyPoints(filename : String) : KeyPoint = {
