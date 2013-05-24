aa9a2ccb9f83142805b7721c367a28b157266bc4
SunPHM <pengyang823@gmail.com>
2013-05-23 21:29:40 -0400

    SIFT features

--- code/src/main/scala/py/Hello.scala
+++ code/src/main/scala/py/Hello.scala
@@ -2,7 +2,6 @@ package py
 
 import com.googlecode.javacv.cpp.opencv_highgui._
 import com.googlecode.javacv.cpp.opencv_core._
-//import com.googlecode.javacv.cpp.opencv_core.CORE
 import com.googlecode.javacv.cpp.opencv_features2d._
 import java.io.File
 import com.googlecode.javacv.CanvasFrame
@@ -12,14 +11,18 @@ import com.googlecode.javacv.CanvasFrame
 import com.googlecode.javacv.cpp.opencv_highgui._
 import javax.swing.JFrame._
 import OpenCVUtils._
+import java.io.PrintWriter
 //import com.googlecode.javacv.cpp.opencv_core.Core
 
 object Hello{
   def main(args:Array[String]){
+    loadFeatures("resources/images")
+  }
+  
+  def extractKeyPoints(filename : String) : KeyPoint = {
     // load image
-    val image = loadAndShowOrExit(new File("boldt.jpg"))
-    
-    // Detect SIFT features.
+    val image = cvLoadImage(filename)
+    // Detect SIFT features
     val keyPoints = new KeyPoint()
     val nFeatures = 0
     val nOctaveLayers = 3
@@ -28,11 +31,33 @@ object Hello{
     val sigma = 1.6
     val sift = new SIFT(nFeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma)
     sift.detect(image, null, keyPoints)
-
+    
+    println(keyPoints)
+    
+    keyPoints
+    
+    // keyPoints.
     // Draw keyPoints
-    val featureImage = cvCreateImage(cvGetSize(image), image.depth(), 3)
-    drawKeypoints(image, keyPoints, featureImage, CvScalar.WHITE, DrawMatchesFlags.DRAW_RICH_KEYPOINTS)
-    show(featureImage, "SIFT Features")
-
+    // val image = loadAndShowOrExit(new File("boldt.jpg"))    
+    // val featureImage = cvCreateImage(cvGetSize(image), image.depth(), 3)
+    // drawKeypoints(image, keyPoints, featureImage, CvScalar.WHITE, DrawMatchesFlags.DRAW_RICH_KEYPOINTS)
+    // show(featureImage, "SIFT Features")
   }
+  
+  def loadFeatures(dir : String)
+  {
+    val folder = new File(dir)
+    val files  = folder.listFiles()
+    val pw = new PrintWriter(new File("resources/features.txt"))
+    files.foreach(f => {
+      println(f.getName())
+      extractKeyPoints(f.getAbsolutePath())
+    })
+  
+  
+  }
+  
+  
 }
+
+
