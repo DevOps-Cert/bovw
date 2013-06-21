be3391a903948c2eb00fb661354a8e01796f6158
SunPHM <pengyang823@gmail.com>
2013-06-20 20:22:01 -0400

    fix

--- code/src/main/scala/py/Extractor.scala
+++ code/src/main/scala/py/Extractor.scala
@@ -16,9 +16,10 @@ import java.io.PrintWriter
 import java.util.ArrayList
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.Date
 
 
-object Extractor{
+object Extractor {
   
   // parameters used to detect SIFT key points
   val nFeatures = 0
@@ -31,35 +32,38 @@ object Extractor{
   
   // open a file to store all the statistics
   val stat = new PrintWriter("resources/stat.txt")
-  // a logger
-  val log = LoggerFactory.getLogger(this.getClass().getName())
+  
   
   def main(args : Array[String]){
+    //val file = new File("resources/oxbuild_images/ashmolean_000214.jpg")
+    //extractFeatures(file)
     run
   }
   
   def run(){
-    log.info("start of run over the Oxford Building 5K dataset")
+    println(new Date().toGMTString() + " " + this.getClass().getName() + " " + "start of run over the Oxford Building 5K dataset")
     // for each file in the directory, extract features, store into files
     val folder = new File("resources/oxbuild_images")
     val files  = folder.listFiles()
     files.foreach(file => extractFeatures(file))
-    log.info("end of run over the Oxford Building 5K dataset")
+    println(new Date().toGMTString() + " " + this.getClass().getName() + " " + "end of run over the Oxford Building 5K dataset")
   }
   
   // read one file and then output the features to one file
   def extractFeatures(file : File){
+    println(new Date().toGMTString() + " " + this.getClass().getName() + " " + file.getName() + ": processing")
     val begin = System.nanoTime()
     // load image
-    val image = cvLoadImageM(file.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE)
-    val keyPoints = new KeyPoint()  
+    var image = cvLoadImageM(file.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE)
+    var keyPoints = new KeyPoint()  
     sift.detect(image, null, keyPoints)
-    val points = toArray(keyPoints)
-    val cv_mat = new CvMat(null)
+    var points = toArray(keyPoints)
+    var cv_mat = new CvMat(null)
     des.compute(image, keyPoints, cv_mat)
     
-    val pw = new PrintWriter("resources/features/" + file.getName() + ".txt")
+    if (cv_mat.isNull()) {println(new Date().toGMTString() + " " + this.getClass().getName() + " " + "empty cv_mat");return} // handling exception
     // write the cv_mat into a file
+    val pw = new PrintWriter("resources/features/" + file.getName() + ".txt")
     for(i <- 0 until cv_mat.rows())
     {
       val row = new ArrayList[Double]
@@ -73,6 +77,14 @@ object Extractor{
     // write the statistics about this image
     stat.println(file.getName() + " " + (end - begin) / 1000000.0 + " " + cv_mat.size() / cv_mat.cols() + " " + cv_mat.cols() + " " + cv_mat.size())
     stat.flush()
-    log.info("" + file.getName() + ": processed")
+    
+    // clear memory and garbage collection
+    image = null
+    keyPoints = null
+    points = null
+    cv_mat = null
+    System.gc()
+    
+    println(new Date().toGMTString() + " " + this.getClass().getName() + " " + file.getName() + ": processed")
   }
 }
