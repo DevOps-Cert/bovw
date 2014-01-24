820f8e81f01116fa89f59816e3394a53fb674e66
Yang Peng <pengyang823@gmail.com>
2014-01-23 21:47:21 -0500

    pipeline online

--- code/src/main/java/py/mahout/TopDownPipeline.java
+++ code/src/main/java/py/mahout/TopDownPipeline.java
@@ -1,89 +1,124 @@
 package py.mahout;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import org.apache.commons.io.FileUtils;
+import java.util.Date;
 
 public class TopDownPipeline {
 	
 	private static String dm = "org.apache.mahout.common.distance.CosineDistanceMeasure";
-	private static double delta = 0.001;
+	private static double delta = 0.1;
 	private static int x = 100;
 	
 	private static int topK = 0;
 	private static int botK = 0;
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, InterruptedException{
 		run(args);
 	}
 	
-	public static void run(String[] args) throws IOException{
+	public static void run(String[] args) throws IOException, InterruptedException{
 		
-		topK = Integer.parseInt(args[0]);
-		botK = Integer.parseInt(args[1]);
+		// parse parameters
+		String data = args[0];
+		String prefix = args[1];
+		String top = prefix + "/top";
+		String mid = prefix + "/mid";
+		String bot = prefix + "/bot";
+		String res = prefix + "/res";
+		topK = Integer.parseInt(args[2]);
+		botK = Integer.parseInt(args[3]);
 		
-		String input = args[2];
-		String cls = args[3];
+		// execute pipeline
+		clean(prefix);
+		topLevelProcess(data, top + "/cls", top, topK);
+		midLevelProcess(top, mid);
+		botLevelProcess(mid, bot, botK, res);
 		
-		String top = args[4];
-		String mid = args[5];
-		String bot = args[6];
-		
-		String res = args[7];
-		
-		topLevel(input, cls, top, topK);
-		//midLevel(top, mid);
-		//botLevel(mid, bot, botK, res);
+		log("top-down clustering pipeline ends");
 	}
 	
-	public static void topLevel(String input, String cls, String top, int topK) throws IOException{
+	public static void clean(String prefix) throws IOException, InterruptedException{
+		String cmd = "hadoop fs -rm -R " + prefix;
+		run(cmd);
+	}
+	
+	public static void topLevelProcess(String input, String cls, String top, int topK) throws IOException, InterruptedException{
 		kmeans(input, cls, top, topK, delta, x);
 	}
 	
-	public static void midLevel(String top, String mid) throws IOException{
-		String command = "mahout clusterpp -i " + top + " -o " + mid;
-		System.out.println(command);
+	public static void midLevelProcess(String top, String mid) throws IOException, InterruptedException{
+		String command = "mahout clusterpp -i " + top + " -o " + mid + " -xm sequential";
+		log(command);
 		run(command);
 	}
 	
-	public static void botLevel(String mid, String bot, int botK, String res) throws IOException{
-		File folder = new File(mid);
-		File[] list = folder.listFiles();
-		//TODO: rewrite here to add hadoop fs results
-		for(int i = 0; i < list.length; i++){
-			// System.out.println(list[i].getName());
-			File file = list[i];
-			kmeans(file.getAbsolutePath(), bot + "/" + file.getName() + "/clusters-0", bot + "/" + file.getName(), botK, delta, x);
-			saveResults(bot + "/" + file.getName() + "/clusters-*-final", res + "/" + file.getName());
+	public static void botLevelProcess(String mid, String bot, int botK, String res) throws IOException, InterruptedException{
+		String[] folders = getFolders(mid);
+		String cmd = "hadoop fs -mkdir " + res;
+		run(cmd);
+		for(int i = 0; i < folders.length; i++){
+			kmeans(folders[i] + "/part-m-0", bot + "/" + i + "/cls", bot + "/" + i, botK, delta, x);
+			copyResults(bot + "/" + i + "/clusters-*-final/*", res + "/" + i);
 		}
 	}
 	
-	public static void kmeans(String input, String clusters, String output, int k, double cd, int x) throws IOException{
-		String command = "mahout kmeans -i " + input + " -c " + clusters + " -o " + output + " -k " + k + " "
-				+ "-dm " + dm + " -cd " + cd + " -x " + x + " -ow -cl" ;
-		System.out.println(command);
-		run(command);
-	}
-	
-	public static void saveResults(String cs, String res) throws IOException{
-		File src = new File(cs);
-		File dst = new File(res);
-		FileUtils.copyDirectory(src, dst);
-		//TODO: probably need to rewritten by using hadoop fs commands
-	}
-	
-	public static void run(String command) throws IOException{
+	public static String[] getFolders(String mid) throws IOException, InterruptedException{
+		
+		String[] folders = new String[botK];
+		int i = -1;
+		String cmd = "hadoop fs -ls " + mid;
+		log(cmd);
+		
 		Runtime rt = Runtime.getRuntime();
-		Process p = rt.exec(command);
-		// output the results
+		Process p = rt.exec(cmd);
 		String line;
 		BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()) );
 	    while ((line = in.readLine()) != null) {
-	    	System.out.println(line);
+	    	if(i == -1) i = 0;
+	    	else{
+	    		folders[i] = line.split(" +")[7];
+	    		i++;
+	    	}
 	    }
 	    in.close();
+		p.waitFor();
+		return folders;
+	}
+	
+	public static void kmeans(String input, String clusters, String output, int k, double cd, int x) throws IOException, InterruptedException{
+		String command = "mahout kmeans -i " + input + " -c " + clusters + " -o " + output + " -k " + k + " "
+				+ "-dm " + dm + " -cd " + cd + " -x " + x + " -ow -cl -xm sequential"  ;
+		log(command);
+		run(command);
+	}
+	
+	public static void copyResults(String cs, String res) throws IOException, InterruptedException{
+		String cmd0 = "hadoop fs -mkdir " + res;
+		run(cmd0);
+		
+		String cmd = "hadoop fs -cp " + cs + " " + res;
+		log(cmd);
+		run(cmd);
+	}
+	
+	public static void log(String msg){
+		Date now = new Date();
+		System.out.println((now.getYear() - 100) + "/" + (now.getMonth() + 1) + "/" + now.getDate() + " " +  
+				now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + ": " + msg);
+	}
+	
+	public static void run(String command) throws IOException, InterruptedException{
+		Runtime rt = Runtime.getRuntime();
+		Process p = rt.exec(command);
+		// output the results
+		// String line;
+		// BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()) );
+	    // while ((line = in.readLine()) != null) {
+	    // // 	System.out.println(line);
+	    //}
+	    //in.close();
+	    p.waitFor();
 	}
 }
