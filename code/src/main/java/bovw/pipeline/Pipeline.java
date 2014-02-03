5ab5bae72686a0d9694af7a2afaf8155b592d69b
Yang Peng <pengyang823@gmail.com>
2014-02-03 17:17:27 -0500

    pipeline setup

---
+++ code/src/main/java/bovw/pipeline/Pipeline.java
@@ -0,0 +1,13 @@
+package bovw.pipeline;
+
+public class Pipeline {
+	
+	public static void main(String args){
+		//TODO: input: a folder containing images
+		//TODO: output: an image retrieval system on GPDB
+		//TODO: all in map/reduce framework?
+		//TODO: how to integrate GPDB on AWB??
+		//TODO: how to use Lucene
+		//TODO: how to build inverted index
+	}
+}
